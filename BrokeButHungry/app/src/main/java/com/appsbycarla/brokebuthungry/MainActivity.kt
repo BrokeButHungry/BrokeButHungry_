a2b8e9bb9f5b7695be8308147aead215fb58b987
CarlaHernandez425 <carla0425@outlook.com>
2023-10-05 22:57:23 -0700

    10.05.23 All Files for BBH

---
+++ BrokeButHungry/app/src/main/java/com/appsbycarla/brokebuthungry/MainActivity.kt
@@ -0,0 +1,282 @@
+/*
+09.09.23 Comments by Carla Ramos.
+Algorithm:
+1. Initialize UI elements, including an EditText for user input, a Button for initiating a search, and a TextView for displaying search results.
+2. Set a click listener for the search button, which triggers an API request when clicked.
+3. In the background thread (AsyncTask FetchRecipesTask):
+    - Construct a URL for an API request using user input and API credentials.
+    - Make an HTTP GET request to the API.
+    - Read and collect the response data, returning it as a string.
+    - Handle exceptions, such as network errors or invalid URLs.
+4. Update the UI on the main thread (in onPostExecute):
+    - Check if the API response is not empty.
+    - If not empty, parse and display the recipe information in the TextView.
+    - If empty, display a "No results found" message in the TextView.
+5. Display recipe information in the TextView by parsing a JSON response, extracting data, and formatting it.
+ */
+
+//MainActivity.kt
+package com.appsbycarla.brokebuthungry
+import android.content.Intent // for search nearby
+import android.os.AsyncTask
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import org.json.JSONArray
+import kotlinx.coroutines.*
+import androidx.lifecycle.lifecycleScope
+
+
+
+class    MainActivity : AppCompatActivity() {
+
+    private lateinit var searchEditText: EditText
+    private lateinit var searchButton: Button
+    private lateinit var recipeNameTextView: TextView
+    private lateinit var caloriesTextView: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Set the content view to the activity's layout defined in activity_main.xml
+        setContentView(R.layout.activity_main)
+
+        // Initialize references to UI elements using their IDs
+        searchEditText = findViewById(R.id.searchEditText)
+        searchButton = findViewById(R.id.searchButton)
+        recipeNameTextView = findViewById(R.id.recipeNameTextView)
+
+        // NEW Search Nearby Groceries Button
+        val searchNearbyButton: Button = findViewById(R.id.btnSearchNearby)
+        searchNearbyButton.setOnClickListener {
+            val intent = Intent(this, SearchNearbyActivity::class.java)
+            intent.putExtra("query", "supermarket")
+            startActivity(intent)
+        }
+
+        // Set a click listener for the searchButton
+        searchButton.setOnClickListener {
+            // Get the user's query from the searchEditText
+            val query = searchEditText.text.toString()
+            // Check if the query is not empty
+            if (query.isNotEmpty()) {
+                // Clear previous results
+                recipeNameTextView.text = "Searching..."
+
+                searchRecipeWithCoroutine(query)
+            }
+        }
+    }
+
+    private fun searchRecipe(query: String): String? {
+        val apiKey = "b3d0fd73ebb946ca9d282a96c16e4b31" // replace with your Spoonacular API key
+        val apiUrl = "https://api.spoonacular.com/recipes/complexSearch?query=$query&apiKey=$apiKey"
+
+        val url = URL(apiUrl)
+        val connection = url.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+
+        if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+            val reader = BufferedReader(InputStreamReader(connection.inputStream))
+            val response = reader.readText()
+            reader.close()
+
+            val jsonResponse = JSONObject(response)
+            val results = jsonResponse.getJSONArray("results")
+
+            // Assuming you want the ID of the first recipe in the search results
+            if (results.length() > 0) {
+                val firstResult = results.getJSONObject(0)
+                return firstResult.getString("id")
+            }
+        }
+        return null
+    }
+
+    private fun searchRecipeWithCoroutine(query: String) {
+        lifecycleScope.launch {
+            try {
+                val recipeId = withContext(Dispatchers.IO) { searchRecipe(query) }
+                if (recipeId != null) {
+                    val recipeInfo = withContext(Dispatchers.IO) { getRecipeInformation(recipeId) }
+                    // Switching to Main thread to update UI
+                    withContext(Dispatchers.Main) {
+                        recipeNameTextView.text = recipeInfo
+                    }
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // Optionally handle errors, maybe update the UI to reflect the error state.
+                withContext(Dispatchers.Main) {
+                    recipeNameTextView.text = "Error occurred: ${e.message}"
+                }
+            }
+        }
+    }
+
+    private fun getRecipeInformation(recipeId: String): String {
+        val apiKey = "b3d0fd73ebb946ca9d282a96c16e4b31" // replace with your Spoonacular API key
+        val apiUrl = "https://api.spoonacular.com/recipes/$recipeId/information?apiKey=$apiKey"
+
+        try {
+            val url = URL(apiUrl)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                val response = reader.readText()
+                reader.close()
+
+                val jsonResponse = JSONObject(response)
+
+                val ingredientsArray = jsonResponse.getJSONArray("extendedIngredients")
+                val instructions = jsonResponse.getString("instructions")
+
+                val sb = StringBuilder()
+
+                for (i in 0 until ingredientsArray.length()) {
+                    val ingredient = ingredientsArray.getJSONObject(i)
+                    val ingredientInfo = ingredient.getString("original")
+                    sb.append(ingredientInfo).append("\n")
+                }
+
+                sb.append("\nInstructions:\n").append(instructions)
+
+                return sb.toString()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return "Error retrieving recipe information."
+    }
+
+    private fun getRecipeInformationWithCoroutine(recipeId: String) {
+        lifecycleScope.launch {
+            try {
+                val recipeInfo = withContext(Dispatchers.IO) { getRecipeInformation(recipeId) }
+                // If you need to update UI with recipeInfo, do it here.
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    inner class FetchRecipesTask : AsyncTask<String, Void, String>() {
+
+        // This method is executed in the background thread.
+        override fun doInBackground(vararg params: String?): String {
+            // Extract the query from the task parameters.
+            val query = params[0]
+
+            // API credentials (app ID and app key) for accessing the Edamam API.
+            val apiId = "456948c8"
+            val apiKey = "da192cac6c51d23c7025297c6c6f78b4"
+
+            // Construct the URL for the API request using the query and credentials.
+            val apiUrl = "https://api.edamam.com/search?q=$query&app_id=$apiId&app_key=$apiKey"
+
+            try {
+                // Create a URL object from the constructed URL string.
+                val url = URL(apiUrl)
+
+                // Open a connection to the URL as an HttpURLConnection.
+                val connection = url.openConnection() as HttpURLConnection
+
+                // Set the request method to "GET."
+                connection.requestMethod = "GET"
+
+                // Get the HTTP response code from the server.
+                val responseCode = connection.responseCode
+
+                // Check if the response code indicates a successful response (HTTP OK).
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    // Read the response data from the input stream.
+                    val reader = BufferedReader(InputStreamReader(connection.inputStream))
+                    val response = StringBuilder()
+                    var line: String?
+
+                    // Read each line of the response and append it to the StringBuilder.
+                    while (reader.readLine().also { line = it } != null) {
+                        response.append(line)
+                    }
+                    // Close the reader.
+                    reader.close()
+
+                    // Return the response data as a string.
+                    return response.toString()
+                }
+            } catch (e: Exception) {
+                // Handle exceptions, such as network errors or invalid URLs.
+                e.printStackTrace()
+            }
+            // If there was an error or no data was received, return an empty string.
+            return ""
+        }
+
+        // This method is executed on the main UI thread after doInBackground completes.
+        override fun onPostExecute(result: String) {
+            if (result.isNotEmpty()) {
+                // If the result is not empty, call the displayResults method to show the data.
+                displayResults(result)
+            } else {
+                // If there are no results, display a message in the resultsTextView.
+                recipeNameTextView.text = "No results found."
+            }
+        }
+    }
+
+    private fun displayResults(jsonResult: String) {
+        // Locate the TextView elements in your activity
+        val recipeNameTextView = findViewById<TextView>(R.id.recipeNameTextView)
+
+        try {
+            // Parse the incoming JSON string into a JSONObject
+            val jsonObject = JSONObject(jsonResult)
+
+            // Get the "hits" array from the JSONObject
+            val hitsArray = jsonObject.getJSONArray("hits")
+
+            // Create a StringBuilder to store all recipe names and calories
+            val recipeInfo = StringBuilder()
+
+            // Loop through each item in the "hits" array
+            for (i in 0 until hitsArray.length()) {
+                // Get the "recipe" object for the current item
+                val recipeObject = hitsArray.getJSONObject(i).getJSONObject("recipe")
+
+                // Extract relevant data from the "recipe" object
+                val recipeLabel = recipeObject.getString("label")
+                val recipeCalories = recipeObject.getDouble("calories")
+
+                // Append the recipe name and calories to the recipeInfo StringBuilder
+                recipeInfo.append("$recipeLabel\nCalories: $recipeCalories\n\n")
+            }
+
+            // Set the combined recipe names and calories in the recipeNameTextView
+            recipeNameTextView.text = recipeInfo.toString()
+
+            // Set the text color to white
+            recipeNameTextView.setTextColor(ContextCompat.getColor(this, android.R.color.white))
+
+            // Adjust the text size for recipe name (if needed)
+            recipeNameTextView.textSize = resources.getDimension(R.dimen.recipe_label_text_size)
+        } catch (e: Exception) {
+            // Handle exceptions, such as JSON parsing errors
+            e.printStackTrace()
+            // Display an error message in the TextView if an exception occurs
+            recipeNameTextView.text = "Error parsing JSON."
+        }
+    }
+}
+
---
+++ BrokeButHungry/app/src/main/java/com/appsbycarla/brokebuthungry/SearchNearbyActivity.kt
@@ -0,0 +1,251 @@
+// SearchNearbyActivity.kt
+
+package com.appsbycarla.brokebuthungry
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.io.BufferedInputStream
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+
+
+class SearchNearbyActivity : AppCompatActivity() {
+
+    lateinit var placesClient: PlacesClient
+    lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.search_nearby)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, "AIzaSyCZR0gVZBwoIod0xP9P_0TWI4PUb4Wfr9A")
+        }
+
+        placesClient = Places.createClient(this)
+
+        val query = intent.getStringExtra("query") ?: return  // Adjusted position
+
+        val backButton: Button = findViewById(R.id.btnBack)
+        backButton.setOnClickListener {
+            finish()
+        }
+
+        // Check if the app has location permission
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        } else {
+            Log.d("PlacesAPI", "Query: $query")
+//            findNearbyPlaces(query)
+            findNearbyPlaces()
+        }
+    }
+
+    private fun findNearbyPlaces() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            location?.let {
+                val latitude = it.latitude
+                val longitude = it.longitude
+                val radius = 10000 // Define the radius in which you want to search places
+
+                val apiKey = "AIzaSyCZR0gVZBwoIod0xP9P_0TWI4PUb4Wfr9A" // Replace with your actual API key
+                val urlString = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" +
+                        "location=$latitude,$longitude" +
+                        "&radius=$radius" +
+                        "&type=supermarket" +
+                        "&key=$apiKey"
+
+                Log.d("Nasaan Ako Tangina", "Request: $urlString")
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        val url = URL(urlString)
+                        val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+                        connection.requestMethod = "GET"
+                        val inputStream: InputStream = BufferedInputStream(connection.inputStream)
+                        val response = inputStream.bufferedReader().use { it.readText() }  // defaults to UTF-8
+                        connection.disconnect()
+
+                        val jsonObject = JSONObject(response)
+                        val results = jsonObject.getJSONArray("results")
+                        val stringBuilder = StringBuilder()
+
+                        for (i in 0 until results.length()) {
+                            val place = results.getJSONObject(i)
+                            val name = place.getString("name")
+                            val vicinity = place.getString("vicinity")
+                            stringBuilder.append("$name, $vicinity\n")
+                        }
+
+                        withContext(Dispatchers.Main) {
+                            val resultsTextView: TextView = findViewById(R.id.nearbyGroceryView)
+                            resultsTextView.text = stringBuilder.toString()
+                        }
+                    } catch (e: Exception) {
+                        Log.e("PlacesAPI", "Error: $e")
+                    }
+                }
+            } ?: run {
+                Log.e("LocationError", "Location is null")
+            }
+        }.addOnFailureListener { exception ->
+            Log.e("LocationError", "Error getting location: $exception")
+        }
+    }
+
+
+//    private fun findNearbyPlaces(query: String) {
+//        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+//            location?.let {
+//                val userLatitude = location.latitude
+//                val userLongitude = location.longitude
+//                val lowerLeftLatitude = userLatitude - 0.01 // Example offset
+//                val lowerLeftLongitude = userLongitude - 0.01 // Example offset
+//                val upperRightLatitude = userLatitude + 0.01 // Example offset
+//                val upperRightLongitude = userLongitude + 0.01 // Example offset
+//
+//                Log.d("Location", "Latitude: $userLatitude, Longitude: $userLongitude")
+//
+//                val bounds = RectangularBounds.newInstance(
+//                    LatLng(lowerLeftLatitude, lowerLeftLongitude),
+//                    LatLng(upperRightLatitude, upperRightLongitude)
+//                )
+//
+//                val placeFields: List<Place.Field> = listOf(Place.Field.NAME, Place.Field.ADDRESS, Place.Field.TYPES)
+//
+//                val placeTypes: MutableList<String> = mutableListOf(
+//                    Place.Type.GROCERY_OR_SUPERMARKET.name,
+//                    Place.Type.SUPERMARKET.name
+//                )
+//
+//                val request: FindAutocompletePredictionsRequest =
+//                    FindAutocompletePredictionsRequest.builder()
+//                        .setLocationBias(bounds)
+//                        //.setTypesFilter(placeTypes)
+//                        .setQuery(query)
+//                        .build()
+//
+//                Log.d("PlacesAPI", "Request: $request")
+//
+//                if (ActivityCompat.checkSelfPermission(
+//                        this,
+//                        Manifest.permission.ACCESS_FINE_LOCATION
+//                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                        this,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION
+//                    ) != PackageManager.PERMISSION_GRANTED
+//                ) {
+//                    ActivityCompat.requestPermissions(
+//                        this,
+//                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+//                        LOCATION_PERMISSION_REQUEST_CODE
+//                    )
+//                    return@addOnSuccessListener
+//                }
+//
+//                placesClient.findAutocompletePredictions(request).addOnSuccessListener { response ->
+//                    val resultsTextView: TextView = findViewById(R.id.nearbyGroceryView)
+//                    val stringBuilder = StringBuilder()
+//                    for (prediction in response.autocompletePredictions) {
+//                        placesClient.fetchPlace(FetchPlaceRequest.newInstance(prediction.placeId, placeFields))
+//                            .addOnSuccessListener { fetchPlaceResponse ->
+//                                val place = fetchPlaceResponse.place
+//                                val types = place.types
+//                                if (types != null && types.contains(Place.Type.GROCERY_OR_SUPERMARKET)) {
+//                                    stringBuilder.append("${place.name}, ${place.address}\n")
+//                                    resultsTextView.text = stringBuilder.toString()
+//                                }
+//                                Log.d("PlacesAPI", "Place Types: $types")
+//                            }.addOnFailureListener { exception ->
+//                                if (exception is ApiException) {
+//                                    Log.e("PlacesAPI", "Place not found: ${exception.statusCode}")
+//                                }
+//                            }
+//                    }
+//                }.addOnFailureListener { exception ->
+//                    Log.e("PlacesAPI", "Error finding places: $exception")
+//                }
+//            } ?: run {
+//                Log.e("LocationError", "Location is null")
+//                // Handle the case where location is null
+//            }
+//        }.addOnFailureListener { exception ->
+//            Log.e("LocationError", "Error getting location: $exception")
+//        }
+//    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            val query = intent.getStringExtra("query") ?: return // Adjusted position
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, proceed with your functionality
+                findNearbyPlaces() //add query later
+            } else {
+                // Permission denied, you might want to display a message to the user.
+                // You might want to disable features that depend on this permission.
+            }
+        }
+    }
+
+    // This function will contain your logic to find places
+    private fun findPlaces() {
+        // Your existing logic to find places
+    }
+
+    companion object {
+        const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+}
